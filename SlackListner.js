8665a2ed14b1db80a52f42530ad2586d5af896ab
Bassel <elbassel.n13@gmail.com>
2019-03-07 09:39:23 +0000

    Catch go keyword in slack messages

--- SlackListner.js
+++ SlackListner.js
@@ -1,24 +1,19 @@
 const { RTMClient } = require('@slack/client');
 
-// An access token (from your Slack app or custom integration - usually xoxb)
-const token = 'xoxp-568680968291-568680970387-568100171952-48e3aacf0257fdc2bca4de4751ab69d6'//process.env.SLACK_TOKEN;
+const token = 'xoxp-568680968291-568680970387-571047345558-49e6e8f30a36a4015ea776d5aa43f150'//process.env.SLACK_TOKEN;
 
-// The client is initialized and then started to get an active connection to the platform
+const GO = 'go';
+
 const rtm = new RTMClient(token);
 rtm.start();
 
 console.log('RTM started');
 rtm.on('message', (message) => {
-    // For structure of `message`, see https://api.slack.com/events/message
-    console.log(JSON.stringify(message));
-    console.log(1);
-    // Skip messages that are from a bot or my own user ID
-    if ( (message.subtype && message.subtype === 'bot_message') ||
-        (!message.subtype && message.user === rtm.activeUserId) ) {
-        console.log(2);
-        return;
+    if(doesMessageContainsKeyword(message.text, GO)) {
+        console.log(message.text);
     }
-    console.log(3);
-    // Log the message
-    console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
 });
+
+function doesMessageContainsKeyword(messageText, keyword) {
+    return messageText.includes(keyword);
+}
